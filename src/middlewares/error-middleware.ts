3119a1dd2e1428d143a9952e5cbfce50129abd6f
lauramarcelli <laura.marcelli@gmail.com>
2025-06-04 12:08:50 -0300

    reordering anc cleaning repo

--- src/middlewares/error-middleware.ts
+++
@@ -1,8 +0,0 @@
-// Importaciones
-import { Request, Response, NextFunction} from 'express';
-
-//Middleware para manejar errores global
-export const errorMiddleware = (err: Error, req: Request, res: Response, next: NextFunction): void =>{
-    console.error('Error: ',err.message);
-    res.status(500).json({ error: 'Algo ocurrió con el servidor' });
-}
